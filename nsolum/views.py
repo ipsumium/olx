53f4a1bbe610ebd6307cd50d4515af85a4a4aec5
ipsumium <ionescu.paul@ymail.com>
2020-11-05 12:30:46 +0200

    v3

--- nsolum/views.py
+++ nsolum/views.py
@@ -1,68 +1,83 @@
 from django.shortcuts import render
-from django import forms
+# from django import forms
 from django.utils.translation import ugettext as _
 from django.shortcuts import redirect
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from bs4 import BeautifulSoup
-import http.cookies
-import urllib.request
+# import http.cookies
+# import urllib.request
 import requests
 import mysql.connector
-import json
-  
-def olx_soup_append(kkeyword,link,results):
-    prom_res = {}
-    soup_main = BeautifulSoup(urllib.request.urlopen(link), features="html.parser")
+# import json
+from multiprocessing import Pool
+
+def parselinks(word):
+    results=[]
+    try:
+        kkeyword=word['word']
+        link = requests.get(word['url'], headers={'User-Agent': 'Mozilla/5.0'})
+        results.extend(olx_soup(kkeyword,link))
+    except:
+        print("An exception occurred-main loop")
+    return results
+
+                 
+def olxhtmlparser(kkeyword,soup_main):
+    results=[]
+    prom_res={}
     soup = soup_main.find('table', {'class': 'fixed offers breakword redesigned'})
     if soup:
         soup = soup.find_all('tr', {'class': 'wrap'})
         for obj in soup:
+            prom_res['keyword'] = kkeyword
             try:
-                prom_res['nrno'] = len(results)+1
-                prom_res['keyword'] = kkeyword
-
-                try:
-                    prom_res['title'] = obj.select('a.marginright5.link.linkWithHash')[0].get_text().replace('\n','').strip(' ')
-                except:
-                    prom_res['title']="Title Error-Fix Me"
-
-                try:
-                    prom_res['image_link'] = obj.find('img', {'class': 'fleft'})['src'].replace('\n','').strip(' ')
-                except:
-                    prom_res['image_link']="https://gmtools.co.uk/wp-content/uploads/2014/12/blank-banner-200x200.jpg"
-                
-                try:
-                    prom_res['app_link'] = obj.select('a.marginright5.link.linkWithHash')[0].get('href')
-                except:
-                    prom_res['app_link']="app_link Error-Fix Me"
-
-                try:
-                    prom_res['price'] = obj.find('p', {'class': 'price'}).get_text().replace('\n','').strip(' ')
-                    prom_res['sortprice'] = int(prom_res['price'].replace(" ", "")[:-1])
-                except:
-                    prom_res['price']="priceERR"
-                    prom_res['sortprice']=9900000
-                    print("price error")
-
-                try:
-                    prom_res['location'] = obj.find('div', {'class': 'space rel'}).find('p',{'class':'lheight16'}, recursive=False).get_text().replace('\n','').strip(' ')
-                except:
-                    prom_res['location']="location Error-Fix Me"
-                                
-                if prom_res != {}:
-                    results.append(prom_res)
-                prom_res = {}                    
+                prom_res['title'] = obj.select('a.marginright5.link.linkWithHash')[0].get_text().replace('\n','').strip(' ')
             except:
-                print("Error in table loop")
-                pass
-        try:
-            nextlink = soup_main.select('a[data-cy="page-link-next"]')[0]
-            print("trying next page, counter=",len(results))
-            olx_soup_append(kkeyword,nextlink.get('href'),results)
-            del nextlink
-        except:
-            print("no next page found")
-            pass
+                prom_res['title']="Title Error-Fix Me"
+
+            try:
+                prom_res['image_link'] = obj.find('img', {'class': 'fleft'})['src'].replace('\n','').strip(' ')
+            except:
+                prom_res['image_link']="https://gmtools.co.uk/wp-content/uploads/2014/12/blank-banner-200x200.jpg"
+            
+            try:
+                prom_res['app_link'] = obj.select('a.marginright5.link.linkWithHash')[0].get('href')
+            except:
+                prom_res['app_link']="app_link Error-Fix Me"
+
+            try:
+                prom_res['price'] = obj.find('p', {'class': 'price'}).get_text().replace('\n','').strip(' ')
+                prom_res['sortprice'] = int(prom_res['price'].replace(" ", "")[:-1])
+            except:
+                prom_res['price']="priceERR"
+                prom_res['sortprice']=9900000
+                print("price error")
+            try:
+                prom_res['location'] = obj.find('div', {'class': 'space rel'}).find('p',{'class':'lheight16'}, recursive=False).get_text().replace('\n','').strip(' ')
+            except:
+                prom_res['location']="location Error-Fix Me"
+            results.append(prom_res)
+            prom_res={}
+    return results
+
+def olx_soup(kkeyword,link):
+    subresults=[]
+    soup_main = BeautifulSoup(link.text, features="html.parser")
+    subresults.extend(olxhtmlparser(kkeyword,soup_main))
+    print("added resutls for keyword=",kkeyword,"-link=",link.url,"\n-status=",link)
+    
+    try:
+        nextlink = soup_main.select('a[data-cy="page-link-next"]')[0]
+        print("trying next page")
+        nextlinklink = requests.get(nextlink.get('href'), headers={'User-Agent': 'Mozilla/5.0'})
+        # soup_main_next=BeautifulSoup(nextlinklink.text, features="html.parser")
+        subresults.extend(olx_soup(kkeyword,nextlinklink))
+        del nextlink
+    except:
+        print("no next page found")
+        pass                             
+    return subresults
+
 
 def search_page(request):
     al = []
@@ -80,17 +95,19 @@ def search_page(request):
         key_words['word'] = line
         key_words['url'] = data2+str(line.strip('\n'))+data3
         al.append(key_words)
-    print(al)
+
+    # for word in al:
+    #     results.extend(parselinks(word))        
+
+    p = Pool(10)  # Pool tells how many at a time
+    mpresults = p.map(parselinks, al)
+    p.terminate()
+    p.join()
+
+    for elem in mpresults:
+        results.extend(elem)
 
 
-    for word in al:
-        try:
-            kkeyword=word['word']
-            link = urllib.request.Request(word['url'], headers={'User-Agent': 'Mozilla/5.0'})
-            olx_soup_append(kkeyword,link,results)
-        except:
-            print("An exception occurred-main loop")
-        
     return render(request, 'results.html', {'results': results, 'rang': len(results)})
 
 def form_search_page(request):
